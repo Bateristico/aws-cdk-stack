6b13c5585bc8aebadd8a67372e14630e337d3c97
Bateristico <camilo.saa@gmail.com>
2021-05-23 19:05:23 +0200

    initial commit

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,3 +1,4 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class SimpleAppStack extends cdk.Stack {
@@ -5,5 +6,15 @@ export class SimpleAppStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // create an s3 bucket
+    const bucket = new Bucket(this, 'MySimpleAppBucket', {
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'MySimpleAppBucketName'
+    });
   }
 }
