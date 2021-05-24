6665a0dc72e0232f41d04ea18de41a9bd5d3254f
Bateristico <camilo.saa@gmail.com>
2021-05-24 21:03:00 +0200

    deploy assets to s3 bucket

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -1,7 +1,10 @@
-import { APIGatewayProxyEventV2, Context, APIGatewayProxyResultV2 } from 'aws-lambda'
+import { APIGatewayProxyEventV2, Context, APIGatewayProxyResultV2 } from 'aws-lambda';
+
+const bucketName = process.env.PHOTO_BUCKET_NAME;
 
 
 async function getPhotos(event: APIGatewayProxyEventV2, context: Context):Promise<APIGatewayProxyResultV2>{
+    console.log("I got the bucket name: "+bucketName);
     return {
         statusCode: 200,
         body: 'Hello from lambda, it is alive!',
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -3,6 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import * as cdk from '@aws-cdk/core';
 import { Runtime } from '@aws-cdk/aws-lambda';
 import * as path from 'path';
+import {BucketDeployment, Source} from '@aws-cdk/aws-s3-deployment'
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,12 +16,22 @@ export class SimpleAppStack extends cdk.Stack {
       encryption: BucketEncryption.S3_MANAGED,
     });
 
+    new BucketDeployment(this, 'MySimpleAppPhotos',{
+      sources: [
+        Source.asset(path.join(__dirname, '..', 'photos'))
+      ],
+      destinationBucket: bucket
+    })
+
     // create lambda
     const getPhotosLambda = new lambda.NodejsFunction(this, 'get-photos', {
       runtime: Runtime.NODEJS_12_X,
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
       handler: 'getPhotos',
-    })
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName,
+      },
+    });
 
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
       value: bucket.bucketName,
